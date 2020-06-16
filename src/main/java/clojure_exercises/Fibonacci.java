a683552dbe864021f43d8e93155f9c35bb06faf2
Mark Bastian <markbastian@gmail.com>
2020-06-16 11:52:03 -0600

    Update Fibonacci.java

--- src/main/java/clojure_exercises/Fibonacci.java
+++ src/main/java/clojure_exercises/Fibonacci.java
@@ -1,6 +1,32 @@
 package clojure_exercises;
 
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
 public class Fibonacci {
+    private final BigInteger current;
+    private final BigInteger next;
+
+    private Fibonacci(BigInteger current, BigInteger next) {
+        this.current = current;
+        this.next = next;
+    }
+
+    private BigInteger current() {
+        return current;
+    }
+
+    private Fibonacci next() {
+        return new Fibonacci(next, current.add(next));
+    }
+
+    private static final Fibonacci SEED = new Fibonacci(BigInteger.ZERO, BigInteger.ONE);
+
+    public static Stream<BigInteger> sequence(){
+        return Stream.iterate(SEED, Fibonacci::next).map(Fibonacci::current);
+    }
+    
+    //Old-style implementation
     public static long fibonacci(long n){
         long i = 0;
         long j = 1;
